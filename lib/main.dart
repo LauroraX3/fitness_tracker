2e1066f024391abc885fb81344b259ad0009e116
laurorax3 <laurorax3@gmail.com>
2024-01-01 18:10:56 +0100

    feature: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness_tracker/app.dart';
+import 'package:fitness_tracker/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initializeDateFormatting();
   runApp(const App());
 }
